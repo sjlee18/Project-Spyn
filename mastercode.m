d47105bf2083dda7a13c3df7c1d5ab44d0887e66
Andy Huynh <115195444+andyh10@users.noreply.github.com>
2022-10-18 15:33:43 -0700

    Update mastercode.m

--- mastercode.m
+++ mastercode.m
@@ -1,9 +1,38 @@
-brick.SetColorMode(4, 4);
-color_rgb = brick.ColorRGB(4);
-% make it work through loop
-% distance sensor turning | 
-while brick.ColorRGB(1) == 0
-    brick.MoveMotor('A', 20);
-    brick.MoveMotor('D', 20);
+% brick = ConnectBrick('NAME');
+% DisconnectBrick('NAME');
+brick.SetColorMode(2, 2);
+% 1 = black
+% 2 = 
+% 3 = green
+% 4 = yellow
+% 5 = red
+
+while 1
     pause(1);
+    color = brick.ColorCode(2);
+    if color == 1 % Go on black, put method or code for maze here
+        brick.MoveMotor('A', -10);
+        brick.MoveMotor('D', -10);
+        if (wall)
+            turn
+    else if color == 5 % Stop on red, continue after 3 seconds
+        brick.StopMotor('A');
+        brick.StopMotor('D');
+        pause (3)
+        brick.MoveMotor('A', -10);
+        brick.MoveMotor('D', -10);
+
+    % Stop on Pick up Color | Need Color Code
+    % else if color == _
+    % brick.StopMotor('A');
+    % brick.StopMotor('D');
+
+    % Finish on color | Need Color code 
+    % else if color == _ 
+    % pause(1)
+    % brick.StopMotor('A');
+    % brick.StopMotor('D');
+
+
+    end 
 end
