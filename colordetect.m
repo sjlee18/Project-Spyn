5988dd7827b9503ce0822751fdbdee767163d30e
Andy Huynh <115195444+andyh10@users.noreply.github.com>
2022-11-10 16:08:10 -0700

    Delete colordetect.m

--- colordetect.m
+++
@@ -1,7 +0,0 @@
-brick.SetColorMode(4, 4);
-color_rgb = brick.ColorRGB(4);
-% get color of object
-fprintf("\tRed: %d\n", color_rgb(1));
-fprintf("\tGreen: %d\n", color_rgb(2));
-fprintf("\tBlue: %d\n", color_rgb(3));
-
