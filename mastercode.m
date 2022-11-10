487a8ce31e5e4bfe42d066c63e2b923fdf688405
Andy Huynh <115195444+andyh10@users.noreply.github.com>
2022-11-10 16:06:44 -0700

    Update mastercode.m

--- mastercode.m
+++ mastercode.m
@@ -1,51 +1,94 @@
 % brick = ConnectBrick('NAME');
 % DisconnectBrick('NAME');
-distance = 0;
-brick.SetColorMode(2, 2);
-touch 
 % 1 = black
 % 2 = blue
 % 3 = green
 % 4 = yellow
 % 5 = red
+global pickUp
+pickUp = false;
+distance = 0;
+brick.SetColorMode(2, 2);
 
 while 1
-    pause(0.5);
     color = brick.ColorCode(2);
-    if color == 1           % Go on black, put method or code for maze here
+    pause(0.1);
+    if color == 5               % Stop on red, continue after 1 seconds
+       brick.StopAllMotors();
+       pause (1);
+       brick.MoveMotor('A', 20);
+       brick.MoveMotor('D', 20);
+       pause (2);
+    elseif color == 3       % stop on green 
+       brick.StopAllMotors();
+       keyboard;
+    elseif color == 2 && pickUp == true % only stop on blue if pickup is true
+       brick.StopAllMotors();
+       keyboard;
+    end
+    if color == 1 || color == 4 || (color == 2 && pickUp == false) % Go on black or yellow or blue
         distance = brick.UltrasonicDist(3);
         touch = brick.TouchPressed(1);
-        if touch                           % if button is pressed turn left
-           brick.StopMotor('A');
-           brick.StopMotor('D'); 
+        if touch                          % if button is pressed turn left
+           brick.MoveMotor('A', -30);
+           brick.MoveMotor('D', -30); 
+           pause(1.5);
+           brick.MoveMotor('A', -25);
+           brick.MoveMotor('D', 25);
+           pause(1.5);
+           brick.MoveMotor('A', 30);
+           brick.MoveMotor('D', 30); 
+           pause(1);
+           brick.StopAllMotors();
+    elseif distance > 50                    % if right is open, turn right
+           brick.MoveMotor('A', 25);
+           brick.MoveMotor('D', 25);
+           pause(1.5);
+           brick.StopAllMotors('Brake');
+           brick.MoveMotor('A', 25); 
+           brick.MoveMotor('D', -25);
+           pause(1.5);
+           scanGround = 0;
+               while scanGround <= 15
+                  color = brick.ColorCode(2); 
+                  brick.MoveMotor('A', 30);
+                  brick.MoveMotor('D', 30);
+                  pause(0.1);
+                  if color == 5
+                      brick.StopAllMotors('Brake');
+                      pause (1);
+                      brick.MoveMotor('A', 20);
+                      brick.MoveMotor('D', 20);
+                      pause(2);
+                      break
+                  end
+                  scanGround = scanGround + 1;
+               end
+               color = brick.ColorCode(2);
+        elseif distance < 10                   % if too close to wall
+           brick.StopAllMotors();
+           pause(0.5);
+           brick.MoveMotor('D', -20);
+           pause(1.0);
+           brick.StopMotor('D');
+           brick.MoveMotor('A', -20);
+           pause(1.6);
+        elseif distance > 25 && distance < 35
+            brick.StopAllMotors();
+            pause(0.5);
+            brick.MoveMotor('A', -20);
+            pause(1);
+            brick.StopMotor('A');
+            brick.MoveMotor('D', -20);
+            pause(1.4);
+            brick.MoveMotor('A', 25); 
+            brick.MoveMotor('D', 25);
+            pause(0.5);
         end
-     %   if distance > 30                    % if right is open, turn right
-      %     brick.MoveMotor('A', -25); % turn left
-       %    brick.MoveMotor('D', 25);
-        %   pause(1.3);
-         %  brick.StopMotor('A');
-          % brick.StopMotor('D'); 
-      %  end
-        brick.MoveMotor('A', -30);
-        brick.MoveMotor('D', -30);
-    elseif color == 5 % Stop on red, continue after 3 seconds
-        brick.StopMotor('A');
-        brick.StopMotor('D');
-        pause (3);
-        brick.MoveMotor('A', -15);
-        brick.MoveMotor('D', -15);
-        pause (3);
-    % Stop on Pick up Color | Need Color Code
-    % else if color == _
-    % brick.StopMotor('A');
-    % brick.StopMotor('D');
-
-    % Finish on color | Need Color code 
-    % else if color == _ 
-    % pause(1)
-    % brick.StopMotor('A');
-    % brick.StopMotor('D');
-
-
+        brick.MoveMotor('A', 30.5);
+        brick.MoveMotor('D', 30);
+        pause(0.5);
     end
+
+
 end
